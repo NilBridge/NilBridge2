0cc522ead2f0bb047cc013feef9bbfb5df35e7d1
Sbaoor <2959435045@qq.com>
2022-03-30 13:33:12 +0800

    fix

--- modules/vanilla/index.js
+++ modules/vanilla/index.js
@@ -120,7 +120,7 @@ function get_xboxid(qq) {
 
 function get_player(xboxid){
     let qq = get_qq(xboxid);
-    if(qq==undefined)return;
+    if(qq==undefined) return {join:0,period:0};
     return playerdata[qq];
 }
 
