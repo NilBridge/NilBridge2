d95f4d9bcd5c39d3b6bacf7571ea3acc890cb8dd
Sbaoor <2959435045@qq.com>
2022-04-07 22:20:07 +0800

    添加处理`mobdie`事件

--- modules/vanilla/onWebsocket.js
+++ modules/vanilla/onWebsocket.js
@@ -1,6 +1,7 @@
 const Lang = require('./Lang');
 const langhelper = new Lang('lang.ini');
 const cfg = require("./config.json");
+const mobs = require('./mobs.json');
 function send2Other(ser, mode, pl, t) {
     var txt = '';
     switch (mode) {
@@ -15,53 +16,59 @@ function send2Other(ser, mode, pl, t) {
             break;
     }
     NIL.SERVERS.forEach((v, k) => {
-        if(k != ser)
+        if (k != ser)
             v.sendText(txt);
     });
 }
 
 let times = new Map();
 
-function onws(dt){
+function onws(dt) {
     let data = JSON.parse(dt.message);
-            switch (data.cause) {
-                case 'chat':
-                    NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.chat, langhelper.get('MEMBER_CHAT', dt.server, data.params.sender, data.params.text));
-                    send2Other(dt.server, data.cause, data.params.sender,data.params.text);
-                    NIL.EventManager.on('onPlayerChat', dt);
-                    break;
-                case 'join':
-                    times.set(data.params.sender,new Date().getTime());
-                    NIL._vanilla.add_time(data.params.sender,0,1);
-                    NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.chat, langhelper.get('MEMBER_JOIN', dt.server, data.params.sender,NIL._vanilla.get_player(data.params.sender).join));
-                    send2Other(dt.server, data.cause, data.params.sender);
-                    NIL.EventManager.on('onPlayerJoin', dt);
-                    break;
-                case 'left':
-                    NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.chat, langhelper.get('MEMBER_LEFT', dt.server, data.params.sender));
-                    send2Other(dt.server, data.cause, data.params.sender);
-                    NIL.EventManager.on('onPlayerLeft', dt);
-                    if(times.has(data.params.sender)){
-                        NIL._vanilla.add_time(data.params.sender,1,new Date().getTime() - times.get(data.params.sender));
-                        times.delete(data.params.sender);
-                    }
-                    break;
-                case 'start':
-                    NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.main, langhelper.get("SERVER_START", dt.server));
-                    NIL.EventManager.on('onServerStart', dt);
-                    break;
-                case 'stop':
-                    NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.main, langhelper.get("SERVER_STOP", dt.server));
-                    NIL.EventManager.on('onServerStop', dt);
-                    break;
-                case "accident_stop":
-                    NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.main, langhelper.get("SERVER_STOP_ACCIDENT", dt.server));
-                    NIL.EventManager.on('onServerAccidentStop', dt);
-                    break;
-                case 'plantext':
-                    NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.main, data.params.text);
-                    break;
+    switch (data.cause) {
+        case 'chat':
+            NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.chat, langhelper.get('MEMBER_CHAT', dt.server, data.params.sender, data.params.text));
+            send2Other(dt.server, data.cause, data.params.sender, data.params.text);
+            NIL.EventManager.on('onPlayerChat', dt);
+            break;
+        case 'join':
+            times.set(data.params.sender, new Date().getTime());
+            NIL._vanilla.add_time(data.params.sender, 0, 1);
+            NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.chat, langhelper.get('MEMBER_JOIN', dt.server, data.params.sender, NIL._vanilla.get_player(data.params.sender).join));
+            send2Other(dt.server, data.cause, data.params.sender);
+            NIL.EventManager.on('onPlayerJoin', dt);
+            break;
+        case 'left':
+            NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.chat, langhelper.get('MEMBER_LEFT', dt.server, data.params.sender));
+            send2Other(dt.server, data.cause, data.params.sender);
+            NIL.EventManager.on('onPlayerLeft', dt);
+            if (times.has(data.params.sender)) {
+                NIL._vanilla.add_time(data.params.sender, 1, new Date().getTime() - times.get(data.params.sender));
+                times.delete(data.params.sender);
             }
+            break;
+        case 'start':
+            NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.main, langhelper.get("SERVER_START", dt.server));
+            NIL.EventManager.on('onServerStart', dt);
+            break;
+        case 'stop':
+            NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.main, langhelper.get("SERVER_STOP", dt.server));
+            NIL.EventManager.on('onServerStop', dt);
+            break;
+        case "accident_stop":
+            NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.main, langhelper.get("SERVER_STOP_ACCIDENT", dt.server));
+            NIL.EventManager.on('onServerAccidentStop', dt);
+            break;
+        case 'plantext':
+            NIL.bots.getBot(cfg.self_id).sendGroupMsg(cfg.group.main, data.params.text);
+            break;
+        case 'mobdie':
+            var mob = "entity." + data.params.srctype.toLowerCase() + ".name";
+            if(mobs[mob] != undefined){
+                NIL.bots.getBot(cfg.self_id).sendGroupMsg(langhelper.get('MEMBER_KILL_BY_MOBS',dt.server,data.params.mobname,mobs[mob]));
+            }
+            break;
+    }
 }
 
 module.exports = onws;
